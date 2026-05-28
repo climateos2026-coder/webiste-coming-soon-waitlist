b0465b5d20329d1ecac5cba6f2a0e78f11a94c94
the-blue-pirates-tbp <advaybagaria1@gmail.com>
2026-05-28 22:33:03 +0530

    fix(security): resolve overly strict CSP blocking Next.js hydration and fonts

--- next.config.ts
+++ next.config.ts
@@ -12,8 +12,9 @@ if (supabaseUrl) {
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' https://plausible.io;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://plausible.io;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' data: https://fonts.gstatic.com;
   img-src 'self' data: https:;
   connect-src 'self' https://api.brevo.com https://plausible.io https://${supabaseHostname};
   frame-src 'self' https://docs.google.com;
